6a9382198339a252c53ee861e8fde58d8a1bf80f
Pascal Collins <pascal.collins@gmx.de>
2020-12-05 22:51:58 +0100

    adds pagination for blog list

---
+++ src/templates/blog-list.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {graphql, Link} from 'gatsby';
+import BlogPost from '../components/BlogPost';
+import Layout from '../components/Layout';
+import styles from './blog-list.module.csss';
+
+export default function BlogListTemplate({data, pageContext}) {
+    const previousPage = pageContext.currentPage === 2 ?
+        '/blog':
+        `/blog/${pageContext.currentPage + 1}`;
+
+    return (
+        <Layout>
+            <div id={styles.hero}>
+                <h1>The Coffee Blog</h1>
+            </div>
+            <main className={styles.blogList}>
+                {data.allMarkdownRemark.edges.map(node => (
+                    <BlogPost
+                        key={node.node.id}
+                        slug={node.node.fields.slug}
+                        title={node.node.frontMatter.title}
+                        date={node.node.frontMatter.date}
+                        excerptt={node.node.excerpt} />
+                ))}
+            </main>
+            <div id={styles.pageLinks}>
+                {pageContext.currentPage > 1 && (
+                    <Link to={previousPage}>
+                     &lt;&lt; Previous Page
+                    </Link>
+                )}
+
+                {pageContext.currentPage < pageContext.pageCount && (
+                    <Link to={nextPage}>
+                        Next Page &gt;&gt;
+                    </Link>
+                )}
+            </div>
+
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query BlogListQuery($skip: Int!, $limit: Int!) {
+        allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+            filter: { frontmatter: { contentKey: { eg: "blog" }}}
+            limit: $limit
+            skip: $skip
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        date(formatString: "MMMM D, YYYY")
+                    }
+                    fields {
+                        slug
+                    }
+                    excerpt
+                }
+            }
+        }
+    }
+`;
