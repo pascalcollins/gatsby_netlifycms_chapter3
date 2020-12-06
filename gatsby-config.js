66ac77815d0d4b12320b9a4fa52fd80328aa9065
Pascal Collins <pascal.collins@gmx.de>
2020-12-06 15:37:48 +0100

    adds an index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,25 @@
 module.exports = {
-  siteMetadata: {
-    title: 'The Coffee Blog'
-  },
+    siteMetadata: {
+        title: 'The Coffee Blog'
+    },
     plugins: [
         'gatsby-plugin-netlify-cms',
         {
-          resolve: 'gatsby-source-filesystem',
-          options: {
-            name: 'blog',
-            path: 'src/blog'
-          }
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'blog',
+                path: 'src/blog'
+            }
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pageData',
+                path: 'src/pageData'
+            },
+
         },
         'gatsby-transformer-remark'
     ]
-};
+}
+;
