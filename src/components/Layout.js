a68bd26ee0b6a64d18b754ec6f09c12a3c3ca339
Pascal Collins <pascal.collins@gmx.de>
2020-12-08 08:32:04 +0100

    increases width of header image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
     {
         file(relativePath: {eq: "coffee.jpg"}){
             childImageSharp {
-                fluid {
+                fluid(maxWidth: 1980) {
                     ...GatsbyImageSharpFluid
                 }
             }
